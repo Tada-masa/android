b6b4c49ca6e3e81d1e2e2732becbf853a846cccb
Tada-masa <tada.onepiece.masa@gmail.com>
2025-04-23 10:38:42 +0900

    first commit

---
+++ app/src/test/java/jp_ac_thers_s_6044_86p_fn/myapplication/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package jp_ac_thers_s_6044_86p_fn.myapplication;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
