32ea960bef79435d5f1c26e734b2326411b97963
Tada-masa <tada.onepiece.masa@gmail.com>
2025-04-30 20:13:26 +0900

    0430 event commit

--- app/src/main/java/jp_ac_thers_s_6044_86p_fn/myapplication/MainActivity.java
+++ app/src/main/java/jp_ac_thers_s_6044_86p_fn/myapplication/MainActivity.java
@@ -1,6 +1,10 @@
 package jp_ac_thers_s_6044_86p_fn.myapplication;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.view.View.OnClickListener;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +12,7 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +24,15 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        Button b = (Button) findViewById(R.id.button4);
+        b.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        CharSequence text = "終了";
+        int duration = Toast.LENGTH_SHORT;
+        Toast.makeText(this/*MyActivity*/,text,duration).show();
+        finish();
     }
 }
